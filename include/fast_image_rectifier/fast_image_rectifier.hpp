c44b4bef7c5981b221a3ae773c6af81a44e882ee
Nguyen Quang <nqoptik@gmail.com>
2021-02-26 01:01:55 +0800

    Fix CMakeLists.txt for OpenCV 4

--- include/fast_image_rectifier/fast_image_rectifier.hpp
+++ include/fast_image_rectifier/fast_image_rectifier.hpp
@@ -20,6 +20,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/CameraInfo.h>
 
+#include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
